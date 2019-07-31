d1b8d139500cc69415407cd72ef6a6f469ed307d
Michele <berselli.michele@gmail.com>
2019-07-31 18:00:08 -0400

    added pipeline_cwl_steps, : to tab in integrity-check.sh

--- dockerfiles/cgap-docker/integrity-check.sh
+++ dockerfiles/cgap-docker/integrity-check.sh
@@ -4,9 +4,9 @@ filename=$1
 
 if samtools quickcheck $filename;
 then
-	echo "quickcheck: OK" > integrity_check
+	echo -e "quickcheck\tOK" > integrity_check
 	reads_number=`samtools view -c $filename`
-	echo "number of reads: $reads_number" >> integrity_check 
+	echo -e "number_of_reads\t$reads_number" >> integrity_check 
 else
-	echo "quickcheck: FAILED" > integrity_check
+	echo -e "quickcheck\tFAILED" > integrity_check
 fi
