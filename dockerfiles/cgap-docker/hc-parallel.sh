61c8279b04a62fe67c650565b5ade4fc47de114d
Soo Lee <duplexa@gmail.com>
2019-09-19 15:12:29 -0400

    Update hc-parallel.sh

--- dockerfiles/cgap-docker/hc-parallel.sh
+++ dockerfiles/cgap-docker/hc-parallel.sh
@@ -2,10 +2,10 @@
 
 inputbam=$1
 reffa=$2
-regionfile=$3
-
-outdir=out_gvcfs
+Xms=$3  # '8g'
+ERC=$4  #'GVCF'
+outdir=$5  # '.'
 
 mkdir -p $outdir
-cat $regionfile | parallel --jobs 24 "gatk PrintReads --java-options '-Xms2g' -I $inputbam --interval-padding 500 -L {} -O {}.sharded.bam; if [[ -e {}.sharded.bam ]]; then gatk HaplotypeCaller --java-options '-Xms8g' -R $reffa -O $outdir/out_full.{}.g.vcf.gz -I {}.sharded.bam -L {} -ERC GVCF --max-alternate-alleles 3 --read-filter OverclippedReadFilter -stand-call-conf 0; fi; rm {}.sharded.ba*"
+gatk HaplotypeCaller --java-options "-Xms$Xms" -R $reffa -O $outdir/out_full.g.vcf.gz -I $inputbam -ERC $ERC --max-alternate-alleles 3 --read-filter OverclippedReadFilter
 
