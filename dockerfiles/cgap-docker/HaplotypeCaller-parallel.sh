9705600c42726a3c6de45eb4b00a8fc7cc7db286
Michele <berselli.michele@gmail.com>
2019-10-30 16:43:43 -0400

    updated parallel with --halt, fixed minor bug when merging headers

--- dockerfiles/cgap-docker/HaplotypeCaller-parallel.sh
+++ dockerfiles/cgap-docker/HaplotypeCaller-parallel.sh
@@ -15,7 +15,7 @@ directory=GVCFS/
 mkdir -p $directory
 
 # runnning HaplotypeCaller in parallel
-cat $regionfile | parallel --jobs $nthreads "gatk PrintReads --java-options '-Xms2g' -I $inputbam --interval-padding 500 -L {} -O {}.sharded.bam; if [[ -e {}.sharded.bam ]]; then gatk HaplotypeCaller --java-options '-Xms8g' -R $reference -O ${directory}out.{}.g.vcf -I {}.sharded.bam -L {} -ERC $ERC --max-alternate-alleles 3 --read-filter OverclippedReadFilter -stand-call-conf $threshold; fi; rm {}.sharded.ba*"
+cat $regionfile | parallel --halt 2 --jobs $nthreads "gatk PrintReads --java-options '-Xms2g' -I $inputbam --interval-padding 500 -L {} -O {}.sharded.bam; if [[ -e {}.sharded.bam ]]; then gatk HaplotypeCaller --java-options '-Xms8g' -R $reference -O ${directory}out.{}.g.vcf -I {}.sharded.bam -L {} -ERC $ERC --max-alternate-alleles 3 --read-filter OverclippedReadFilter -stand-call-conf $threshold; fi; rm {}.sharded.ba*" || exit 1
 
 # merging the results
 array=(${directory}*g.vcf)
@@ -23,20 +23,20 @@ array=(${directory}*g.vcf)
 IFS=$'\n' sorted=($(sort -V <<<"${array[*]}"))
 unset IFS
 
-grep "^##" ${sorted[0]} > combined.gvcf
+grep "^#" ${sorted[0]} > combined.gvcf
 
 for filename in ${sorted[@]};
   do
     if [[ $filename =~ "M" ]]; then
       chr_M=$filename
     else
-      grep -v "^##" $filename >> combined.gvcf
+      grep -v "^#" $filename >> combined.gvcf
       rm -f $filename ${filename}.idx
     fi
   done
 
 if [[ -v  chr_M  ]]; then
-  grep -v "^##" $chr_M >> combined.gvcf
+  grep -v "^#" $chr_M >> combined.gvcf
   rm -f $chr_M ${chr_M}.idx
 fi
 
