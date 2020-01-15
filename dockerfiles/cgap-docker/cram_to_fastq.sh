5666b491015a942c52659f15f363aac7ebd32cee
SooLee <duplexa@gmail.com>
2020-01-15 10:04:47 -0500

    minor fix

--- dockerfiles/cgap-docker/cram_to_fastq.sh
+++ dockerfiles/cgap-docker/cram_to_fastq.sh
@@ -70,6 +70,6 @@ samtools view -@$nthreads -hb -T $ref_fasta $input_cram > $bam || { echo "cannot
 # bam to fastq
 mkfifo 1.fastq
 mkfifo 2.fastq
+gzip -c 1.fastq > $out_prefix.1.fastq.gz &
+gzip -c 2.fastq > $out_prefix.2.fastq.gz &
 { samtools collate -@$nthreads -O $bam | samtools fastq -1 1.fastq -2 2.fastq -@$nthreads - } || { echo "cannot convert bam to fastq."; exit 1; }
-gzip -c 1.fastq > $out_prefix.1.fastq.gz
-gzip -c 2.fastq > $out_prefix.2.fastq.gz
