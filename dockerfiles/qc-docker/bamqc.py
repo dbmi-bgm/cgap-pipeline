d5970fad40d33ac39d84056c1e8f95996e8d0b75
SooLee <duplexa@gmail.com>
2020-08-07 11:44:01 -0400

    typos

--- dockerfiles/qc-docker/bamqc.py
+++ dockerfiles/qc-docker/bamqc.py
@@ -13,7 +13,7 @@ def main():
                         default=1,
                         help='number of processes to use')
     parser.add_argument('-t|--tmpprefix',
-                        default='tmp-'
+                        default='tmp-',
                         help='prefix for tmp files, default tmp-')
     parser.add_argument('-g|--eff-genome-size',
                         default=2913022398,  # GRCh38 number of non-N bases
@@ -26,7 +26,7 @@ def main():
 
     # output dictionary
     output = dict()
-    output{'mapping stats'} = dict()
+    output['mapping stats'] = dict()
 
     # first collate bam by reads
     if args.skip_collate:
@@ -46,22 +46,22 @@ def main():
     command_mapped_singletons = 'samtools view -F 0x4 -f 0x8 %s | cut -f1 | uniq | wc -l' % collated_bam
     command_both_unmapped = 'samtools view -f12 %s | cut -f1 | uniq | wc -l' % collated_bam
 
-    stats = output{'mapping stats'}
-    stats{'total reads'} = subprocess.check_output(command_total_reads, shell=True)
-    stats{'both mates mapped'} = subprocess.check_output(command_both_mapped, shell=True)
-    stats{'one mate mapped'} = subprocess.check_output(command_mapped_singletons, shell=True)
-    stats{'neither mate mapped'} = subprocess.check_output(command_both_unmapped, shell=True)
+    stats = output['mapping stats']
+    stats['total reads'] = subprocess.check_output(command_total_reads, shell=True)
+    stats['both mates mapped'] = subprocess.check_output(command_both_mapped, shell=True)
+    stats['one mate mapped'] = subprocess.check_output(command_mapped_singletons, shell=True)
+    stats['neither mate mapped'] = subprocess.check_output(command_both_unmapped, shell=True)
 
-    if stats{'both mates mapped'} + stats{'one mate mapped'} + stats{'neither mate mapped'} != stats{'total reads'}:
+    if stats['both mates mapped'] + stats['one mate mapped'] + stats['neither mate mapped'] != stats['total reads']:
         raise Exception("stats do not match - %s" % str(output))
 
     # read length (assumes constant across file, just use the first read)
     command_read_length = "samtools view %s |head -1 | cut -f10 | tr -d '\n'  | wc -m" % collated_bam
-    stats{'read length'} = subprocess.check_output(command_read_length, shell=True)
+    stats['read length'] = subprocess.check_output(command_read_length, shell=True)
 
     # depth of coverage
-    total_read_coverage = (stats{'both mates mapped'} * 2 + stats{'one mate mapped'}) * stats{'read length'}
-    output{'coverage'} = round(total_read_coverage / args.eff_genome_size, 1)
+    total_read_coverage = (stats['both mates mapped'] * 2 + stats['one mate mapped']) * stats['read length']
+    output['coverage'] = round(total_read_coverage / args.eff_genome_size, 1)
 
     # print output
     with open(args.outprefix + '.stats', 'w') as f:
