4edd7e1bd84ca699a7d287d60aa1fdd5c5cf23d2
SooLee <duplexa@gmail.com>
2020-08-07 16:37:39 -0400

    minor fix in bamqc

--- dockerfiles/qc-docker/bamqc.py
+++ dockerfiles/qc-docker/bamqc.py
@@ -47,17 +47,17 @@ def main():
     command_both_unmapped = 'samtools view -@%d -f12 %s | cut -f1 | uniq | wc -l' % (args.nthreads, collated_bam)
 
     stats = output['mapping stats']
-    stats['total reads'] = subprocess.check_output(command_total_reads, shell=True)
-    stats['both mates mapped'] = subprocess.check_output(command_both_mapped, shell=True)
-    stats['one mate mapped'] = subprocess.check_output(command_mapped_singletons, shell=True)
-    stats['neither mate mapped'] = subprocess.check_output(command_both_unmapped, shell=True)
+    stats['total reads'] = int(subprocess.check_output(command_total_reads, shell=True).decode().rstrip('\n'))
+    stats['both mates mapped'] = int(subprocess.check_output(command_both_mapped, shell=True).decode().rstrip('\n'))
+    stats['one mate mapped'] = int(subprocess.check_output(command_mapped_singletons, shell=True).decode().rstrip('\n'))
+    stats['neither mate mapped'] = int(subprocess.check_output(command_both_unmapped, shell=True).decode().rstrip('\n'))
 
     if stats['both mates mapped'] + stats['one mate mapped'] + stats['neither mate mapped'] != stats['total reads']:
         raise Exception("stats do not match - %s" % str(output))
 
     # read length (assumes constant across file, just use the first read)
     command_read_length = "samtools view %s |head -1 | cut -f10 | tr -d '\n'  | wc -m" % collated_bam
-    stats['read length'] = subprocess.check_output(command_read_length, shell=True)
+    stats['read length'] = int(subprocess.check_output(command_read_length, shell=True).decode().rstrip('\n'))
 
     # depth of coverage
     total_read_coverage = (stats['both mates mapped'] * 2 + stats['one mate mapped']) * stats['read length']
@@ -66,7 +66,7 @@ def main():
     # print output
     with open(args.outprefix + '.stats', 'w') as f:
         json.dump(output, f, indent=4)
-
+        f.write('\n')
 
 if __name__ == '__main__':
     main()
