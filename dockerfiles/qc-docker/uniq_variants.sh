6b543a929111b8e67a4e83e43e354573995210a2
Michele Berselli <berse@MacBook-Pro.local>
2021-03-10 13:13:58 -0500

    .

--- dockerfiles/qc-docker/uniq_variants.sh
+++ dockerfiles/qc-docker/uniq_variants.sh
@@ -7,4 +7,4 @@ vcf_file=$1
 uniq_var=$(gunzip -c $vcf_file | grep -v '^#' | cut -f 1,2 | uniq | wc -l | sed 's/^[ \t]*//;s/[ \t]*$//')
 
 # Printing result as tsv
-echo -e "{'total unique variants in vcf':'${uniq_var}'}" > uniq_variants.json
+echo -e "{\"total unique variants in vcf\":\"${uniq_var}\"}" > uniq_variants.json
