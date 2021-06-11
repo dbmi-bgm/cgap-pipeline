4668264f97bf6896b5bf824f63962f75f2132b75
SooLee <duplexa@gmail.com>
2021-06-11 11:07:44 -0400

    metawfr-ralted stuff (temp)

--- create_metawfr_v22.py
+++ create_metawfr_v22.py
@@ -1,5 +1,7 @@
 import json
-from magma import ff_parser
+import copy
+import uuid
+from magma_ff.metawfl import MetaWorkflow
 from dcicutils import ff_utils
 ff_key = ff_utils.get_authentication_with_server(ff_env='fourfront-cgapwolf')
 
@@ -7,7 +9,7 @@ metawf_uuid = '475a0518-9b72-4dc1-b049-070f366ab821'  # v22
 case_uuid = '1ea344bc-e211-4b11-8bde-7b94dadda7e2'  # NA12879
 
 
-def create_metawfr_template(metawf_uuid, case_uuid, ff_key):
+def create_metawfr_from_case(metawf_uuid, case_uuid, ff_key, post=False, verbose=False):
     case_meta = ff_utils.get_metadata(case_uuid, add_on='?frame=raw', key=ff_key)
     sp_uuid = case_meta['sample_processing']
     sp_meta = ff_utils.get_metadata(sp_uuid, add_on='?frame=object', key=ff_key)
@@ -68,16 +70,29 @@ def create_metawfr_template(metawf_uuid, case_uuid, ff_key):
                #'name': 'MetaWorkflowRun %s for case %s' % (metawf_meta['title'], case_meta['accession']),
                'project': case_meta['project'],
                'institution': case_meta['institution'],
-               'final_status': 'pending'}
+               'common_fields': {'project': case_meta['project'],
+                                 'institution': case_meta['institution']},
+               'final_status': 'pending',
+               'uuid': str(uuid.uuid4())}
+
+    mwf = MetaWorkflow(metawf_meta)
+
+    # get the input structure in magma format
+    metawfr_mgm = MetaWorkflowRun(metawfr).to_json()
+    input_structure = metawfr_mgm['input'][0]['files']
+
+    # create workflow_runs
+    mwfr = mwf.write_run(input_structure)
+    metawfr['workflow_runs'] = mwfr['workflow_runs']
 
     # post meta-wfr
-    res_post = ff_utils.post_metadata(metawfr, 'MetaWorkflowRun', key=ff_key)
-    print(res_post)
+    if post:
+        res_post = ff_utils.post_metadata(metawfr, 'MetaWorkflowRun', key=ff_key)
+        if verbose:
+            print(res_post)
 
-    ff_parser.ParserFF(metawfr).arguments_to_json()
-    ff_parser.ParserFF(metawf_meta).arguments_to_json()
-    mwf = wfl.MetaWorkflow(metawf_meta)
-    mwfr = mwf.write_run(mwf.end_workflows, metawfr['input'][0]['files'])
+    return metawfr
+
 
 def pedigree_to_qc_pedigree(samples_pedigree):
     """extract pedigree for qc for every family member
