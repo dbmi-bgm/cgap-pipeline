340734aa5ec6111d31a12432c7b09e4814271570
Michele Berselli <berse@MacBook-Pro.local>
2021-06-28 17:20:06 -0400

    .

--- post_patch_to_portal.py
+++ post_patch_to_portal.py
@@ -5,7 +5,8 @@ import argparse
 
 
 def main(ff_env='fourfront-cgapwolf', skip_software=False, skip_file_format=False,
-         skip_workflow=False, skip_metaworkflow=False, del_prev_version=False):
+         skip_workflow=False, skip_metaworkflow=False, skip_file_reference=False,
+         del_prev_version=False):
     """post / patch contents from portal_objects to the portal"""
     keycgap = ff_utils.get_authentication_with_server(ff_env=ff_env)
 
@@ -35,22 +36,6 @@ def main(ff_env='fourfront-cgapwolf', skip_software=False, skip_file_format=Fals
             except:
                 ff_utils.patch_metadata(dd, dd['uuid'], key=keycgap)
 
-    # Metaworkflows
-    if not skip_metaworkflow:
-        print("Processing metaworkflow...")
-        wf_dir = "portal_objects/metaworkflows"
-        files = os.listdir(wf_dir)
-
-        for fn in files:
-            if fn.endswith('.json'):
-                print("  processing file %s" % fn)
-                with open(os.path.join(wf_dir, fn), 'r') as f:
-                    d = json.load(f)
-                try:
-                    ff_utils.post_metadata(d, 'MetaWorkflow', key=keycgap)
-                except:
-                    ff_utils.patch_metadata(d, d['uuid'], key=keycgap)
-
     # Workflows
     if not skip_workflow:
         print("Processing workflow...")
@@ -76,6 +61,34 @@ def main(ff_env='fourfront-cgapwolf', skip_software=False, skip_file_format=Fals
                 except:
                     ff_utils.patch_metadata(d, d['uuid'], key=keycgap)
 
+    # File reference
+    if not skip_file_reference:
+        with open('portal_objects/file_reference.json') as f:
+            d = json.load(f)
+
+        for dd in d:
+            print("  processing uuid %s" % dd['uuid'])
+            try:
+                ff_utils.post_metadata(dd, 'FileReference', key=keycgap)
+            except:
+                ff_utils.patch_metadata(dd, dd['uuid'], key=keycgap)
+
+    # Metaworkflows
+    if not skip_metaworkflow:
+        print("Processing metaworkflow...")
+        wf_dir = "portal_objects/metaworkflows"
+        files = os.listdir(wf_dir)
+
+        for fn in files:
+            if fn.endswith('.json'):
+                print("  processing file %s" % fn)
+                with open(os.path.join(wf_dir, fn), 'r') as f:
+                    d = json.load(f)
+                try:
+                    ff_utils.post_metadata(d, 'MetaWorkflow', key=keycgap)
+                except:
+                    ff_utils.patch_metadata(d, d['uuid'], key=keycgap)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--ff-env', default='fourfront-cgapwolf')
@@ -83,8 +96,10 @@ if __name__ == "__main__":
     parser.add_argument('--skip-file-format', action='store_true')
     parser.add_argument('--skip-workflow', action='store_true')
     parser.add_argument('--skip-metaworkflow', action='store_true')
+    parser.add_argument('--skip-file-reference', action='store_true')
     parser.add_argument('--del-prev-version', action='store_true')
     args = parser.parse_args()
     main(ff_env=args.ff_env, skip_software=args.skip_software,
          skip_file_format=args.skip_file_format, skip_workflow=args.skip_workflow,
-         skip_metaworkflow=args.skip_metaworkflow, del_prev_version=args.del_prev_version)
+         skip_metaworkflow=args.skip_metaworkflow, skip_file_reference=args.skip_file_reference,
+         del_prev_version=args.del_prev_version,)
