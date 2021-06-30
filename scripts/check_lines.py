7ad86a5b6fe0940f41226f184737fb441d35c3b1
Michele Berselli <berse@MacBook-Pro.local>
2021-06-30 10:45:01 -0400

    .

--- scripts/check_lines.py
+++ scripts/check_lines.py
@@ -112,6 +112,7 @@ fastqs_dict = {
 def check_lines(metawfr_uuid, ff_key, steps=steps_dict, fastqs=fastqs_dict):
     """
     """
+    print('### ' + metawfr_uuid + '\n')
 
     # Get meta-workflow-run and create MetaWorkflowRun object
     run_json = ff_utils.get_metadata(metawfr_uuid, add_on='?frame=raw&datastore=database', key=ff_key)
@@ -121,95 +122,115 @@ def check_lines(metawfr_uuid, ff_key, steps=steps_dict, fastqs=fastqs_dict):
     # Check fastqs
     for _, run_obj in metawflrun_obj.runs.items():
         count, match_count = 0, 0
-        if run_obj.name in fastqs and run_obj.status == 'completed':
-            # Get output count
-            for output in run_obj.output:
-                if output['argument_name'] == fastqs[run_obj.name]['output']:
-                    output_uuid = output['file']
-                    qc_key = fastqs[run_obj.name]['key']
-                    count = int(get_count_qc(qc_key, output_uuid, ff_key))
-                    break
-                #end if
-            #end for
-            print(run_obj.shard_name + ': ' + str(count))
-
-            # Get input file to match from jobid
-            ffwr_obj = wfrutils.FFWfrUtils(env='env')
-            ffwr_obj._ff_key = ff_key
-            for file in ffwr_obj.wfr_metadata(run_obj.jobid)['input_files']:
-                if file['workflow_argument_name'] in fastqs[run_obj.name]['input_match']:
-                    input_uuid = file['value']['uuid']
-                    qc_key = fastqs[run_obj.name]['key_match']
-                    match_count = int(get_count_fastqc(qc_key, input_uuid, ff_key))
-                    if not count == match_count:
-                        is_match = False
+        if run_obj.name in fastqs:
+            if run_obj.status == 'completed':
+                # Get output count
+                for output in run_obj.output:
+                    if output['argument_name'] == fastqs[run_obj.name]['output']:
+                        output_uuid = output['file']
+                        qc_key = fastqs[run_obj.name]['key']
+                        count = int(get_count_qc(qc_key, output_uuid, ff_key))
+                        break
                     #end if
-                    print(file['workflow_argument_name'] + ': ' + str(match_count))
-                    print('---> ' + str(count == match_count) + '\n')
-                #end if
-            #end for
+                #end for
+                print(run_obj.shard_name + ': ' + str(count))
+
+                # Get input file to match from jobid
+                ffwr_obj = wfrutils.FFWfrUtils(env='env')
+                ffwr_obj._ff_key = ff_key
+                for file in ffwr_obj.wfr_metadata(run_obj.jobid)['input_files']:
+                    if file['workflow_argument_name'] in fastqs[run_obj.name]['input_match']:
+                        input_uuid = file['value']['uuid']
+                        qc_key = fastqs[run_obj.name]['key_match']
+                        match_count = int(get_count_fastqc(qc_key, input_uuid, ff_key))
+                        if not count == match_count:
+                            is_match = False
+                        #end if
+                        print(file['workflow_argument_name'] + ': ' + str(match_count))
+                    #end if
+                #end for
+                print('Matching: ' + str(count == match_count) + '\n')
+            else:
+                print('Missing: ' + run_obj.name + '\n')
+                print('Completed: False\n')
+                return False
+            #end if
         #end if
     #end for
 
     # Check steps
     for _, run_obj in metawflrun_obj.runs.items():
         count, total_count = 0, 0
-        if run_obj.name in steps and run_obj.status == 'completed':
-            # Get output count
-            for output in run_obj.output:
-                if output['argument_name'] == steps[run_obj.name]['output']:
-                    output_uuid = output['file']
-                    qc_key = steps[run_obj.name]['key']
-                    count = int(get_count_qc(qc_key, output_uuid, ff_key))
-                    break
-                #end if
-            #end for
-            print(run_obj.shard_name + ': ' + str(count))
+        if run_obj.name in steps:
+            if run_obj.status == 'completed':
+                # Get output count
+                for output in run_obj.output:
+                    if output['argument_name'] == steps[run_obj.name]['output']:
+                        output_uuid = output['file']
+                        qc_key = steps[run_obj.name]['key']
+                        count = int(get_count_qc(qc_key, output_uuid, ff_key))
+                        break
+                    #end if
+                #end for
+                print(run_obj.shard_name + ': ' + str(count))
 
-            # Get dependencies count
-            for shard_name in run_obj.dependencies:
-                if shard_name.split(':')[0] == steps[run_obj.name]['dependency']:
-                    run_obj_ = metawflrun_obj.runs[shard_name]
-                    for output in run_obj_.output:
-                        if output['argument_name'] == steps[run_obj.name]['output_match']:
-                            output_uuid = output['file']
-                            qc_key = steps[run_obj.name]['key_match']
-                            total_count += int(get_count_qc(qc_key, output_uuid, ff_key))
-                            break
-                        #end if
-                    #end for
-                    print(shard_name + ': ' + str(total_count))
+                # Get dependencies count
+                for shard_name in run_obj.dependencies:
+                    if shard_name.split(':')[0] == steps[run_obj.name]['dependency']:
+                        run_obj_ = metawflrun_obj.runs[shard_name]
+                        for output in run_obj_.output:
+                            if output['argument_name'] == steps[run_obj.name]['output_match']:
+                                output_uuid = output['file']
+                                qc_key = steps[run_obj.name]['key_match']
+                                total_count += int(get_count_qc(qc_key, output_uuid, ff_key))
+                                break
+                            #end if
+                        #end for
+                        print(shard_name + ': ' + str(total_count))
+                    #end if
+                #end for
+                print('Matching: ' + str(count == total_count) + '\n')
+                # Check counts match
+                if not count == total_count:
+                    is_match = False
                 #end if
-            #end for
-            print('---> ' + str(count == total_count) + '\n')
-            # Check counts match
-            if not count == total_count:
-                is_match = False
+            else:
+                print('Missing: ' + run_obj.name + '\n')
+                print('Completed: False\n')
+                return False
             #end if
         #end if
     #end for
-    print('all good? ' + str(is_match))
+    print('Completed: ' + str(is_match) + '\n')
     return is_match
 #end def
 
 def get_count_qc(qc_key, uuid, ff_key):
     """
     """
-    res_json = ff_utils.get_metadata(uuid, add_on='?frame=raw&datastore=database', key=ff_key)
-    qc_uuid = res_json['quality_metric']
-    qc_json = ff_utils.get_metadata(qc_uuid, add_on='?datastore=database', key=ff_key)
-    for qc in qc_json['quality_metric_summary']:
-        if qc['title'] == qc_key:
-            return qc['value']
-        #end if
-    #end for
+    try:
+        res_json = ff_utils.get_metadata(uuid, add_on='?frame=raw&datastore=database', key=ff_key)
+        qc_uuid = res_json['quality_metric']
+        qc_json = ff_utils.get_metadata(qc_uuid, add_on='?datastore=database', key=ff_key)
+        for qc in qc_json['quality_metric_summary']:
+            if qc['title'] == qc_key:
+                return qc['value']
+            #end if
+        #end for
+    except KeyError:
+        return 0
+    #end try
 #end def
 
 def get_count_fastqc(qc_key, uuid, ff_key):
     """
     """
-    res_json = ff_utils.get_metadata(uuid, add_on='?frame=raw&datastore=database', key=ff_key)
-    qc_uuid = res_json['quality_metric']
-    qc_json = ff_utils.get_metadata(qc_uuid, add_on='?datastore=database', key=ff_key)
-    return qc_json[qc_key]
+    try:
+        res_json = ff_utils.get_metadata(uuid, add_on='?frame=raw&datastore=database', key=ff_key)
+        qc_uuid = res_json['quality_metric']
+        qc_json = ff_utils.get_metadata(qc_uuid, add_on='?datastore=database', key=ff_key)
+        return qc_json[qc_key]
+    except KeyError:
+        return 0
+    #end try
 #end def
