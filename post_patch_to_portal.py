50c2bcd4c6b225e41139a5333d1c4cfb7c8f58c2
phil-grayson <>
2021-09-15 15:31:01 -0400

    added functionality for ecr image creation

--- post_patch_to_portal.py
+++ post_patch_to_portal.py
@@ -1,11 +1,12 @@
-import os, sys, argparse
+import os, sys, argparse, subprocess
 import json
 from dcicutils import ff_utils, s3_utils
 import boto3
 
 def main(ff_env='fourfront-cgapwolf', skip_software=False, skip_file_format=False,
          skip_workflow=False, skip_metaworkflow=False, skip_file_reference=False,
-         skip_cwl=False, cwl_bucket='', account='', region='', pipeline='', del_prev_version=False, ignore_key_conflict=False,
+         skip_cwl=False, skip_ecr=False, cwl_bucket='', account='', region='',
+         pipeline='', del_prev_version=False, ignore_key_conflict=False,
          ugrp_unrelated=False, action='store_true'):
     """post / patch contents from portal_objects to the portal"""
 
@@ -183,6 +184,42 @@ def main(ff_env='fourfront-cgapwolf', skip_software=False, skip_file_format=Fals
             print("ERROR: when run without --skip-cwl, user must provide input for:\n    --cwl-bucket (user provided: "+cwl_bucket+")\n    --account (user provided: "+account+")\n    --region (user provided: "+region+")\n    --pipeline (user provied: "+pipeline+" choices are \'snv\' or \'sv\')")
             sys.exit(1)
 
+    if not skip_ecr:
+        print("Processing ECR images...")
+        if account != '' and region != '':
+            account_region = account+".dkr.ecr."+region+".amazonaws.com"
+            cmd = '''
+            # login
+            aws ecr get-login-password --region REGION | docker login --username AWS --password-stdin ACCOUNT
+
+            # cgap on docker is snv on ECR
+            docker pull cgap/cgap:VERSION
+            docker tag cgap/cgap:VERSION ACCOUNT/snv:VERSION
+            docker push ACCOUNT/snv:VERSION
+
+            # md5 is same on docker and ECR
+            docker pull cgap/md5:VERSION
+            docker tag cgap/md5:VERSION ACCOUNT/md5:VERSION
+            docker push ACCOUNT/md5:VERSION
+
+            # fastqc is same on docker and ECR
+            docker pull cgap/fastqc:VERSION
+            docker tag cgap/fastqc:VERSION ACCOUNT/fastqc:VERSION
+            docker push ACCOUNT/fastqc:VERSION
+            '''
+
+            # replace all variables
+            cmd = cmd.replace("REGION", region).replace("ACCOUNT", account_region).replace("VERSION", version)
+
+            # push create and push images
+            subprocess.check_call(cmd, shell=True)
+
+            print("ECR images created!")
+        else:
+            # throw an error if the cwl bucket is not provided
+            print("ERROR: when run without --skip-ecr, user must provide input for:\n    --account (user provided: "+account+")\n    --region (user provided: "+region+")")
+            sys.exit(1)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--ff-env', default='fourfront-cgapwolf')
@@ -192,6 +229,7 @@ if __name__ == "__main__":
     parser.add_argument('--skip-metaworkflow', action='store_true')
     parser.add_argument('--skip-file-reference', action='store_true')
     parser.add_argument('--skip-cwl', action='store_true')
+    parser.add_argument('--skip-ecr', action='store_true')
     parser.add_argument('--cwl-bucket', default='')
     parser.add_argument('--account', default='')
     parser.add_argument('--region', default='')
@@ -204,6 +242,6 @@ if __name__ == "__main__":
     main(ff_env=args.ff_env, skip_software=args.skip_software,
          skip_file_format=args.skip_file_format, skip_workflow=args.skip_workflow,
          skip_metaworkflow=args.skip_metaworkflow, skip_file_reference=args.skip_file_reference,
-         skip_cwl=args.skip_cwl, cwl_bucket=args.cwl_bucket, account=args.account,
+         skip_cwl=args.skip_cwl, skip_ecr=args.skip_ecr, cwl_bucket=args.cwl_bucket, account=args.account,
          region=args.region, pipeline=args.pipeline, del_prev_version=args.del_prev_version,
          ignore_key_conflict=args.ignore_key_conflict, ugrp_unrelated=args.ugrp_unrelated)
