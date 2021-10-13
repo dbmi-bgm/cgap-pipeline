970950e41d4020c2d13720d5d035c4503081d1bd
phil-grayson <>
2021-10-13 17:06:23 -0400

    extension change to HaplotypeCaller.sh

--- dockerfiles/cgap-docker/HaplotypeCaller.sh
+++ dockerfiles/cgap-docker/HaplotypeCaller.sh
@@ -16,11 +16,11 @@ mkdir -p $directory
 
 # runnning HaplotypeCaller
 
-gatk HaplotypeCaller --java-options '-Xms8g' -R $reference -O ${directory}combined.gvcf -I $inputbam -L $regionfile -ERC $ERC --max-alternate-alleles 3 --read-filter OverclippedReadFilter -stand-call-conf $threshold -ip 500 || exit 1
+gatk HaplotypeCaller --java-options '-Xms8g' -R $reference -O ${directory}combined.g.vcf -I $inputbam -L $regionfile -ERC $ERC --max-alternate-alleles 3 --read-filter OverclippedReadFilter -stand-call-conf $threshold -ip 500 || exit 1
 
 # move results
 
-mv ${directory}combined.gvcf . || exit 1
+mv ${directory}combined.g.vcf combined.gvcf || exit 1
 
 # compress and index combined gvcf
 bgzip combined.gvcf || exit 1
